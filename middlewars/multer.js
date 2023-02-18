4f281dd329329e8fcbd0df4c389dbd62d7dc977e
teraclitos <tefans12@gmail.com>
2023-02-18 19:43:17 -0300

    permito archivos temporales

--- middlewars/multer.js
+++ middlewars/multer.js
@@ -1,8 +1,7 @@
 const multer = require(`multer`);
-const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, `../public/uploads/tmp/`),
+  destination: "./tmp/",
   filename: (req, file, cb) => {
     cb(null, uuidv4() + path.extname(file.originalname));
   },
