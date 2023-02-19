cb6b8b5667997b37b08dba3c4c6d62608293e0dd
teraclitos <tefans12@gmail.com>
2023-02-19 10:24:18 -0300

    pruebo corregir error

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require(`express`);
 const app = express();
 const morgan = require(`morgan`);
 const cors = require(`cors`);
-const multer = require(`multer`);
-const { v4: uuidv4 } = require("uuid");
-const path = require("path");
+// const multer = require(`multer`);
+// const { v4: uuidv4 } = require("uuid");
+// const path = require("path");
 
 if (process.env.NODE_ENV !== `production`) {
   require("dotenv").config();
@@ -16,19 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, `/public/uploads`),
-  filename: (req, file, cb) => {
-    cb(null, uuidv4() + path.extname(file.originalname));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: path.join(__dirname, `/public/uploads`),
+//   filename: (req, file, cb) => {
+//     cb(null, uuidv4() + path.extname(file.originalname));
+//   },
+// });
 
 // app.get(`/`, (req, res) => {
 //   res.render(`index`);
 // });
 
 const routes = require("./routes");
-app.use(multer({ storage }).array(`images`, 15));
+// app.use(multer({ storage }).array(`images`, 15));
 
 app.use("/", routes);
 
