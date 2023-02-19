dced0f7c4ec870c40fc40d1b6cb7d6b83d8fc815
teraclitos <tefans12@gmail.com>
2023-02-18 21:11:01 -0300

    pruebo solucionar error

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require(`express`);
 const app = express();
 const morgan = require(`morgan`);
 const cors = require(`cors`);
-const multer = require(`multer`);
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
 
 if (process.env.NODE_ENV !== `production`) {
   require("dotenv").config();
@@ -16,21 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
-// const storage = multer.diskStorage({
-//   destination: path.join(__dirname, `/public/uploads`),
-//   filename: (req, file, cb) => {
-//     cb(null, uuidv4() + path.extname(file.originalname));
-//   },
-// });
-// const upload = multer({ storage }).array(`images`, 15);
-// module.exports = upload;
 
 // app.get(`/`, (req, res) => {
 //   res.render(`index`);
 // });
 
 const routes = require("./routes");
-
+app.use("/public", express.static(path.join(__dirname, `../public/uploads`)));
 app.use("/", routes);
 
 app.listen(PORT, () => {
