8e2e2bb3e6e025bf7ba067663798a2f269b81da1
teraclitos <tefans12@gmail.com>
2023-02-19 12:12:47 -0300

    pruebo algo

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const express = require(`express`);
 const app = express();
 const morgan = require(`morgan`);
 const cors = require(`cors`);
+const path = require(`path`);
 
 if (process.env.NODE_ENV !== `production`) {
   require("dotenv").config();
 }
 require("./dataBase");
 const PORT = process.env.PORT || 3001;
+app.set(`views`, path.join(__dirname, `views`));
+app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
 
-
-// app.get(`/`, (req, res) => {
-//   res.render(`index`);
-// });
+app.get(`/`, (req, res) => {
+  res.render(`index`);
+});
 
 const routes = require("./routes");
-
 
 app.use("/", routes);
 
