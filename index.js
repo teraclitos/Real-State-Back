56c8542074e913b15c79d3da38511d8ae0166a48
teraclitos <tefans12@gmail.com>
2023-02-19 19:57:31 -0300

    agrego express.static

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
-
+app.use("/", express.static(path.join(__dirname, "public/uploads")));
 const routes = require("./routes");
 
 app.use("/", routes);
