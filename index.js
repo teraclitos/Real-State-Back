021e3caeb945ce8d3affbdaef55dd49355f4bd8f
teraclitos <tefans12@gmail.com>
2023-02-19 10:21:30 -0300

    pruebo corregir el error

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require(`express`);
 const app = express();
 const morgan = require(`morgan`);
 const cors = require(`cors`);
+const multer = require(`multer`);
+const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 
 if (process.env.NODE_ENV !== `production`) {
@@ -14,16 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
-app.use(
-  "/public/uploads",
-  express.static(path.join(__dirname, `/public/uploads`))
-);
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, `/public/uploads`),
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname));
+  },
+});
 
 // app.get(`/`, (req, res) => {
 //   res.render(`index`);
 // });
 
 const routes = require("./routes");
+app.use(multer({ storage }).array(`images`, 15));
 
 app.use("/", routes);
 
