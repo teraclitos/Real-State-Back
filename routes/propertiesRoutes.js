931a12bc837791be67ddb8028ea0ce138646fa5a
teraclitos <tefans12@gmail.com>
2023-02-20 12:12:16 -0300

    agrego multer a api

--- routes/propertiesRoutes.js
+++ routes/propertiesRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const propertiesControllers = require("../controllers/propertiesControllers");
 const { body } = require("express-validator");
 const auth = require("../middlewars/auth");
-const upload = require("../middlewars/multer");
+const upload = require("../api/multer");
 
 router.post(
   "/create",
