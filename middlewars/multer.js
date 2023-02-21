17ece4582ae1d45ad53aa6741b56f77f531185de
teraclitos <tefans12@gmail.com>
2023-02-21 17:17:33 -0300

    pruebo solucion al error

--- middlewars/multer.js
+++ middlewars/multer.js
@@ -2,7 +2,9 @@ const multer = require(`multer`);
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, `../public/uploads`),
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, `../public/uploads`));
+  },
   filename: (req, file, cb) => {
     cb(null, uuidv4() + path.extname(file.originalname));
   },
