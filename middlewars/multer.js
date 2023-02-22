e4e78826ba7a65d19dadef8dc151de518fb383bf
teraclitos <tefans12@gmail.com>
2023-02-22 15:12:00 -0300

    corrijo pathfile

--- middlewars/multer.js
+++ middlewars/multer.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 
 module.exports = uploadFile = () => {
   const storage = multer.diskStorage({
-    destination: require.main?.path + "/" + "..public/uploads/",
+    destination: path.join(__dirname, `../public/uploads`),
     filename: (req, file, cb) => {
       cb(null, uuidv4() + path.extname(file.originalname));
     },
