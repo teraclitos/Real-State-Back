bfb29d7cf8a4994f3545635b60535a35228133d4
teraclitos <tefans12@gmail.com>
2023-02-22 17:53:41 -0300

    pruebo algo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./dataBase");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan(`dev`));
 app.use(cors());
 
--- middlewars/multer.js
+++ middlewars/multer.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 
 module.exports = uploadFile = () => {
   const storage = multer.diskStorage({
-    destination: path.join(__dirname, `../public/uploads`),
+    destination: path.join(__dirname, `../tmp`),
     filename: (req, file, cb) => {
       cb(null, uuidv4() + path.extname(file.originalname));
     },
